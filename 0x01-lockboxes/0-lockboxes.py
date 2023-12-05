13238f77d27a362c90e56c85cc9203026beb56d1
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-05 04:22:11 +0300

    Create 0-lockboxes.py
    - Function checks whether all boxes can be opened based on the given list of
      boxes. It traverse through the boxes reachable from the keys found so far

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""
+0-lockboxes Module
+
+Module for the canUnlockAll function to determine if all the boxes can be
+opened.
+
+Usage:
+    The canUnlockAll function takes a list of lists (boxes) as input,
+    where each inner list represents a box, and each box may contain keys
+    to other boxes. It returns True if all boxes can be opened, otherwise
+    False.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+    - boxes (list of lists): A list of lists where each inner list represents
+                             a box. Each box may contain keys to other boxes.
+
+    Returns:
+    - bool: True if all boxes can be opened, False otherwise.
+    """
+    if not boxes or len(boxes) == 0:
+        return False
+
+    keys = [0]  # Start with the keys from box 0
+    visited = set()  # To keep track of visited boxes
+
+    while keys:
+        box = keys.pop()
+        if box not in visited:
+            visited.add(box)
+            keys.extend(boxes[box])
+
+    return len(visited) == len(boxes)
