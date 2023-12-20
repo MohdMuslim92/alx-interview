a5523ccfd13270f4346810bcde98119ff3ee7b5a
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-21 02:37:54 +0300

    Update 0-stats.py
    - Refactor log parsing script and fix errors
    
    - Restructured the script that reads log lines from stdin to enhance
      readability and maintainability
    - Improved parsing logic and error handling for extracting status codes and
      file sizes from log lines.
    - Refactored the `print_statistics` function to display the total file size
      and count of each status code in a more readable and organized format.
    - Addressed potential exceptions more explicitly for better error handling.

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,68 +5,50 @@ based on the specified log format.
 """
 
 import sys
-from collections import defaultdict
-import re
 
 
-def parse_line(line):
-    """
-    Parse the given log line.
+if __name__ == '__main__':
 
-    Args:
-    line (str): Log line to be parsed.
+    total_size, lines_processed = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {i: 0 for i in codes}
 
-    Returns:
-    tuple: A tuple containing the status code and file size if the line matches
-    the expected format, otherwise (None, None).
-    """
-    pattern = (
-        r'^([\d.]+)\s+-\s+\[([^\]]+)\]\s+"(?:GET|POST)\s+.*?"'
-        r'\s+(\d+)\s+(\d+)'
-    )
-    match = re.match(pattern, line)
-    if match:
-        return int(match.group(3)), int(match.group(4))
-    return None, None
+    def print_statistics(stats: dict, total_size: int) -> None:
+        """
+        Print the aggregated statistics based on the accumulated status counts.
 
+        Args:
+        stats (dict): A dictionary containing counts of status codes.
+        total_size (int): Total accumulated file size.
 
-def print_statistics(total_size, status_counts):
-    """
-    Print the aggregated statistics.
-
-    Args:
-    total_size (int): Total accumulated file size.
-    status_counts (dict): Dictionary containing counts of status codes.
-    """
-    print(f'File size: {total_size}')
-    for code in sorted(status_counts.keys()):
-        if status_counts[code] > 0:
-            print(f'{code}: {status_counts[code]}')
-
-
-def main():
-    """
-    Main function to process log lines from stdin and print statistics.
-    """
-    lines_processed = 0
-    total_size = 0
-    status_counts = defaultdict(int)
+        Prints:
+        The total file size and the count of each status code in the given
+        dictionary.
+        Status codes with a count greater than zero are displayed in
+        ascending order.
+        """
+        print(f'File size: {total_size}')
+        for i, j in sorted(stats.items()):
+            if j:
+                print(f'{i}: {j}')
 
     try:
         for line in sys.stdin:
-            line = line.strip()
-            status_code, file_size = parse_line(line)
-            if status_code is not None and file_size is not None:
-                lines_processed += 1
-                total_size += file_size
-                status_counts[str(status_code)] += 1
-
-                if lines_processed % 10 == 0:
-                    print_statistics(total_size, status_counts)
+            lines_processed += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                total_size += int(data[-1])
+            except BaseException:
+                pass
+            if lines_processed % 10 == 0:
+                print_statistics(stats, total_size)
+        print_statistics(stats, total_size)
     except KeyboardInterrupt:
-        print_statistics(total_size, status_counts)
-        sys.exit(0)
-
-
-if __name__ == "__main__":
-    main()
+        print_statistics(stats, total_size)
+        raise
